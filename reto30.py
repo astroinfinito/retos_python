22d17ebdb4bbc4f1d7c8d3288922be6d63234dfa
Alberto Zarzo <stroncio198@gmail.com>
2022-08-29 17:02:41 +0200

    Add files via upload

---
+++ reto30.py
@@ -0,0 +1,27 @@
+#### Este es un reto de programación que pinta divertido####
+####  Enunciado: Crea una función que reciba un texto y muestre cada palabra en una línea, formando
+# * un marco rectangular de asteriscos.
+# * - ¿Qué te parece el reto? Se vería así:
+# *   **********
+# *   * ¿Qué   *
+# *   * te     *
+# *   * parece *
+# *   * el     *
+# *   * reto?  *
+# *   **********
+# *
+
+def funcion():
+
+    frase = "Hola"
+    frase2 = "a "
+    frase3 = "todos"
+    for i in range(3):
+        if i < 3:
+            print(frase,end="\n*")
+            print(frase2,end="\n*")
+            print(frase3,end="\n*")
+        else:
+            print(i)
+
+funcion()
